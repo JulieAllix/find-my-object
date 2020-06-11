b896dd891d9f2e0e535f688a3dd2f37dbd9adbcc
JulieAllix <allixjulie@gmail.com>
2020-06-11 19:24:36 +0200

    Switch Map and Shelf from Class component to Functional component

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -8,35 +8,34 @@ import warehouse from '../../assets/images/warehouse.jpg';
 const letters = ['a', 'b', 'c', 'd', 'e'];
 const numbers = ['0', '1', '2', '3', '4', '5'];
 
-class Map extends React.Component {
-  render() {
-    return (
-      <MapStyled>
-        <div className="map-container">
-          <img
-            src={warehouse}
-            alt="Le plan de l'entrepôt"
-            className="warehouse"
-          />
-          <div className="shelves">
-          {/* I map on the letter and number objects in order to create the shelves names*/}
-            {letters.map((letter) => (
-              <div className="shelves-row">
-                {numbers.map((number) => (
-                  <Shelf 
-                    key={number}
-                    letter={letter}
-                    number={number}
-                    highlightedShelf="b3"
-                  />
-                ))}
-              </div>
-            ))}
-          </div>
+const Map = () => {
+  return (
+    <MapStyled>
+      <div className="map-container">
+        <img
+          src={warehouse}
+          alt="Le plan de l'entrepôt"
+          className="warehouse"
+        />
+        <div className="shelves">
+        {/* I map on the letter and number objects in order to create the shelves names*/}
+          {letters.map((letter) => (
+            <div className="shelves-row">
+              {numbers.map((number) => (
+                <Shelf 
+                  key={number}
+                  letter={letter}
+                  number={number}
+                  highlightedShelf="b3"
+                />
+              ))}
+            </div>
+          ))}
         </div>
-      </MapStyled>
-    );
-  }
-}
+      </div>
+    </MapStyled>
+  );
+};
+
 
 export default Map;
--- src/components/Shelf/index.js
+++ src/components/Shelf/index.js
@@ -3,40 +3,38 @@ import classNames from 'classnames';
 
 import ShelfStyled from './ShelfStyled';
 
-class Shelf extends React.Component {
-  render() {
-    const shelfName = this.props.letter + this.props.number;
-    if (shelfName == this.props.highlightedShelf){
-      return (
-        <ShelfStyled>
-          <div className={classNames({
-            shelf: true,
-            highlighted: true,
-          })}
-          >
-            <p className="shelf-name">
-              {this.props.letter}{this.props.number}
-            </p>
-          </div>
-        </ShelfStyled>
-      );
-    } else {
-      return (
-        <ShelfStyled>
-          <div className={classNames({
-            shelf: true,
-            highlighted: false,
-          })}
-          >
-            <p className="shelf-name">
-              {this.props.letter}{this.props.number}
-            </p>
-          </div>
-        </ShelfStyled>
-      );
-    }
-
+const Shelf = ({letter, number, highlightedShelf}) => {
+  const shelfName = letter + number;
+  if (shelfName == highlightedShelf){
+    return (
+      <ShelfStyled>
+        <div className={classNames({
+          shelf: true,
+          highlighted: true,
+        })}
+        >
+          <p className="shelf-name">
+            {letter}{number}
+          </p>
+        </div>
+      </ShelfStyled>
+    );
+  } else {
+    return (
+      <ShelfStyled>
+        <div className={classNames({
+          shelf: true,
+          highlighted: false,
+        })}
+        >
+          <p className="shelf-name">
+            {letter}{number}
+          </p>
+        </div>
+      </ShelfStyled>
+    );
   }
-}
+};
+
 
 export default Shelf;
