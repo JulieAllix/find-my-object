521672f52091d06d36242481e196d7443a74f4d8
JulieAllix <allixjulie@gmail.com>
2020-06-11 19:27:21 +0200

    Switch Input and Header from Class component to Functional component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,23 +2,19 @@ import React from 'react';
 
 import InputStyled from './InputStyled';
 
-class Input extends React.Component {
-
-  
-  render() {
-    return (
-      <InputStyled>
-        <form className="search-bar-form">
-          <input 
-            className="search-bar-input"
-            placeHolder="ex: dq324"
-            type="text"
-            name="searchBar"
-          />
-        </form>
-      </InputStyled>
-    );
-  }
-}
+const Input = () => {
+  return (
+    <InputStyled>
+      <form className="search-bar-form">
+        <input 
+          className="search-bar-input"
+          placeHolder="ex: dq324"
+          type="text"
+          name="searchBar"
+        />
+      </form>
+    </InputStyled>
+  );
+};
 
 export default Input;
