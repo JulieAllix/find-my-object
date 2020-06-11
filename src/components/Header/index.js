80a55ae87c2dc72541044025ee1ed035261da34a
JulieAllix <allixjulie@gmail.com>
2020-06-11 16:35:36 +0200

    Add logo, create title and instructions. Basic style, will be improved later.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import logo from '../../assets/images/stockoss.png';
+
 import HeaderStyled from './HeaderStyled';
 
 import { render } from '@testing-library/react';
@@ -10,7 +12,18 @@ class Header extends React.Component {
   render() {
     return (
       <HeaderStyled>
-        Header
+        <img
+          src={logo}
+          alt="Le logo de Stockoss"
+          className="logo"
+        />
+        <div className="title-container">
+          Où est mon objet ?
+        </div>
+        <div className="instructions-container">
+          <p className="instructions-paragraph">Trouvez l'emplacement d'un objet dans l'entrepôt en temps réel !</p> 
+          <p className="instructions-paragraph">Saisissez l'identifiant (cinq caractères) de l'objet que vous cherchez dans la barre de recherche ci-dessous. Son emplacement apparaîtra sur la carte de l'entrepôt.</p>
+        </div>
       </HeaderStyled>
     );
   }
