70135c75a7c7b6ed0084729a21d981aac0ae11ab
JulieAllix <allixjulie@gmail.com>
2020-06-12 16:08:37 +0200

    Solve display issue on IE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
