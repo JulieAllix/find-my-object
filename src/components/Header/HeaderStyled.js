50c9ea5b7e222ccc530c6ee84a5e05545ea67723
JulieAllix <allixjulie@gmail.com>
2020-06-12 09:38:07 +0200

    Add picture to header and style title

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 import theme from '../../styles/theme';
 
+import warehouse from '../../assets/images/warehouse-pic.jpg';
+
 const HeaderStyled = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     margin: 2rem auto;
+    
+
+    .warehouse {
+        background-image: url(${warehouse});
+        background-position: center;
+        background-size: cover;
+        margin-top: 4rem;
+        height: 180px;
+        width: 100%;
+    }
 
     .title-container {
-        margin: 2rem;
+        margin: 2rem 0;
         padding: 2rem;
+        display: inline-block;
         background-color: ${theme.accentColor};
         color: white;
         font-weight: bold;
+        font-size: ${theme.mobileTitle};
     }
 
     .instructions-container {
@@ -28,11 +42,30 @@ const HeaderStyled = styled.div`
     }
 
     @media screen and (min-width: 600px){
-    
+
+        .warehouse {
+            height: 300px;
+        }
+
+        .title-container {
+            margin: 4rem 0;
+            padding: 3rem;
+            font-size: ${theme.tabletTitle};
+        }
     }
 
     @media screen and (min-width: 900px){
 
+        .warehouse {
+            height: 500px;
+        }
+
+        .title-container {
+            margin: 8rem 0;
+            padding: 3.5rem;
+            font-size: ${theme.desktopTitle};
+        }
+        
     }
     
 `
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,9 +8,12 @@ const theme = {
 
     primaryColor: '#314e58',
     accentColor: '#222222',
-    backgroundColor: '#f3f9ff'
+    backgroundColor: '#f3f9ff',
 
     /* Sizes */
+    mobileTitle: '1.5rem',
+    tabletTitle: '2rem',
+    desktopTitle: '3rem',
     
 };
 
